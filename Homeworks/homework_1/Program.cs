cf4ee3a1b72e7e7d59415eebc9bffdbb5387f691
Mikhail <misha.anisimov.2012@mail.ru>
2022-11-19 01:41:09 +0400

    Задача№2. Решено

--- Homeworks/homework_1/Program.cs
+++ Homeworks/homework_1/Program.cs
@@ -4,3 +4,23 @@ a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3*/
 
 
+Console.WriteLine("Введите первое число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 < num2)
+{
+    Console.WriteLine($"{num1} < {num2}");
+}
+else
+{
+    Console.WriteLine($"{num1} > {num2}");
+}
+
+
+/*Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22*/
+
+
