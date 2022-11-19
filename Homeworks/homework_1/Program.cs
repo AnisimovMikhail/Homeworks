cd89329bdb5f9aa701b07cb6af0cb4c9e6998de0
Mikhail <misha.anisimov.2012@mail.ru>
2022-11-19 15:55:58 +0400

    Задача№6.Решено

--- Homeworks/homework_1/Program.cs
+++ Homeworks/homework_1/Program.cs
@@ -3,7 +3,6 @@ a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3*/
 
-
 /*Console.WriteLine("Введите первое число ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число ");
@@ -18,13 +17,13 @@ else
 }*/
 
 
+
 /*Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 2, 3, 7 -> 7
 44 5 78 -> 78
 22 3 9 -> 22*/
 
-
-Console.WriteLine("Введите первое число ");
+/*Console.WriteLine("Введите первое число ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число ");
 int num2 = Convert.ToInt32(Console.ReadLine());
@@ -39,4 +38,23 @@ if (num3 > max)
 {
     max = num3;
 }
-Console.WriteLine($"Maximum number of -> {max}");
+Console.WriteLine($"Maximum number of -> {max}");*/
+
+
+
+/*Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет*/
+
+
+Console.WriteLine("Введите четное число ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if(numberA % 2 == 1)
+{
+    Console.WriteLine($"{numberA} -> НЕТ, не является");
+}
+else
+{
+    Console.WriteLine($"{numberA} -> ДА, является"); 
+}
