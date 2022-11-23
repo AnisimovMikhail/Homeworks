57154b26d2ca04ce0a20c9c3a6f8a2a62fbed7de
Mikhail <misha.anisimov.2012@mail.ru>
2022-11-23 14:35:58 +0400

    Zadacha21.Resheno

--- Homeworks/homework_3/Program.cs
+++ Homeworks/homework_3/Program.cs
@@ -3,14 +3,59 @@
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
 
-void CubicNum (int numN)
+// void CubicNum (int numN)
+// {
+//     Console.WriteLine($"Кубические числа от 1 до {numN}");
+//     for (int i = 1; i < numN + 1; i++)
+//     {
+//         Console.Write($" |{i} = {i * i * i}| ");
+//     }
+// }
+// Console.WriteLine("Введите число ");
+// int result = Convert.ToInt32(Console.ReadLine());
+// CubicNum (result);
+
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+
+
+
+
+
+
+
+
+
+
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+void DistansPoint(double x1, double y1, double z1, double x2, double y2, double z2)
 {
-    Console.WriteLine($"Кубические числа от 1 до {numN}");
-    for (int i = 1; i < numN + 1; i++)
-    {
-        Console.Write($" |{i} = {i * i * i}| ");
-    }
+    double distX = x2 - x1;
+    double distY = y2 - y1;
+    double distZ = z2 - z1;
+    double XYZ = Math.Sqrt(distX * distX + distY * distY + distZ * distZ);
+    Console.WriteLine($"Расстояние между точками {XYZ} ");
+    
 }
-Console.WriteLine("Введите число ");
-int result = Convert.ToInt32(Console.ReadLine());
-CubicNum (result);
+Console.WriteLine("Задайте координату X ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Задайте координату Y ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Задайте координату Z ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Задайте координату X1 ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Задайте координату Y1 ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Задайте координату Z1 ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+DistansPoint(x1, y1, z1, x2, y2, z2);
+
