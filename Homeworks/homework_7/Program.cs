21c8aaf2271f56ab2ee1b0182ec48861517a676d
Mikhail <misha.anisimov.2012@mail.ru>
2022-12-03 21:41:38 +0400

    homework7.Resheno

---
+++ Homeworks/homework_7/Program.cs
@@ -0,0 +1,172 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+// double[,]  Create2DRandomArray (int minValue, int maxValue, int rows, int columns)
+// {
+//     double[,] newArray = new double[rows, columns];
+//     for (int i = 0; i < rows; i++)
+//     {
+//         for (int j = 0; j < columns; j++)
+//         {
+//              newArray[i,j] = new Random().NextDouble() * (maxValue - minValue) + minValue;//Next(minValue, maxValue + 1);
+//             // Console.WriteLine(Math.Round(newArray[i,j],1));
+//         }
+//     }
+//     return newArray;
+// }
+
+// void Show2DDoubleArray (double [,] array)
+// {
+//    for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write($"{Math.Round(array[i,j],2)} ");
+//         }
+//         Console.WriteLine();
+//     } 
+// }
+// Console.WriteLine("Введите количество строк ");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов ");
+// int columns = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите минимальное значение элемента ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите максимальное значение элемента ");
+// int max = Convert.ToInt32(Console.ReadLine());
+// Show2DDoubleArray(Create2DRandomArray(min, max, rows, columns));
+
+
+
+
+
+// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+// int[,] Create2DRandomArray (int minValue, int maxValue, int rows, int columns)
+// {
+//     int[,] newArray = new int[rows,columns];
+//     for (int i = 0; i < rows; i++)
+//     {
+//         for (int j = 0; j < columns; j++)
+//         {
+//             newArray[i,j] = new Random().Next(minValue,maxValue+1);
+//         }
+//     }
+//     return newArray;
+// }
+
+// void ShowArray (int [,] array)
+// {
+//    for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i,j]+" ");
+//         }
+//         Console.WriteLine();
+//     } 
+// }
+
+// void ArithmeticMean (int [,] array)
+// {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//         double sum = 0;
+//         for (int i = 0; i < array.GetLength(0); i++)
+//         {
+//             sum += array[i,j];
+//         }
+//         Console.Write($"{Math.Round(sum / array.GetLength(0),2)} ");
+//         // Console.Write($"{ sum / array.GetLength(0)} ");
+//     }  
+//     Console.WriteLine();  
+// }
+
+// Console.WriteLine("Введите количество строк ");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов ");
+// int columns = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите минимальное значение элемента ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите максимальное значение элемента ");
+// int max = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine();
+// int [,] myArray = Create2DRandomArray(min,max,rows,columns);
+// ShowArray(myArray);
+// Console.WriteLine();
+// ArithmeticMean(myArray);
+
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 17 -> такого числа в массиве нет
+
+
+int[,] Create2DRandomArray (int minValue, int maxValue, int rows, int columns)
+{
+    int[,] newArray = new int[rows,columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            newArray[i,j] = new Random().Next(minValue,maxValue+1);
+        }
+    }
+    return newArray;
+}
+
+void ShowArray (int [,] array)
+{
+   for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j]+" ");
+        }
+        Console.WriteLine();
+    } 
+}
+
+void ReturnElement (int [,] array, int row, int column)
+{
+    if(row > array.GetLength(0) || column > array.GetLength(1))
+    {
+        Console.WriteLine($"Элемент [{row}, {column}] не существует");
+    }
+    else Console.WriteLine($"Элемент [{row}, {column}] -> {array [row, column]}");
+}
+
+
+Console.WriteLine("Введите количество строк ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов ");
+int columns = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальное значение элемента ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное значение элемента ");
+int max = Convert.ToInt32(Console.ReadLine());
+int [,] myArray = Create2DRandomArray(min,max,rows,columns);
+ShowArray(myArray);
+Console.WriteLine("Введите индекс строки ");
+int myRow = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите индекс столбца ");
+int myColumns = Convert.ToInt32(Console.ReadLine());
+ReturnElement(myArray,myRow,myColumns);
--- Seminars/Seminar9/Program.cs
+++ Seminars/Seminar9/Program.cs
@@ -38,49 +38,49 @@
 //M = 1; N = 5. -> "1, 2, 3, 4, 5"
 //M = 4; N = 8. -> "4, 5, 6, 7, 8"
 
-// void findInterval(int start, int finish)
-// {
-//     if (start != finish)
-//     {
-//         if ( start > finish)
-//         {
-//             findInterval(start - 1, finish);
-//             Console.Write(start + " ");
+void findInterval(int start, int finish)
+{
+    if (start != finish)
+    {
+        if ( start > finish)
+        {
+            findInterval(start - 1, finish);
+            Console.Write(start + " ");
 
-//         }
-//         else
-//         {
-//             findInterval(start, finish - 1);
-//             Console.Write(finish + " ");
+        }
+        else
+        {
+            findInterval(start, finish - 1);
+            Console.Write(finish + " ");
 
-//         }
-//     }
-//     else Console.Write(start + " ");
-// }
+        }
+    }
+    else Console.Write(start + " ");
+}
 
-// Console.WriteLine("Введите начало промежутка ");
-// int start = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите конец промежутка ");
-// int finish = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите начало промежутка ");
+int start = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите конец промежутка ");
+int finish = Convert.ToInt32(Console.ReadLine());
 
-// findInterval(start, finish);
+findInterval(start, finish);
 
 
 //Задача 4.Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
 //A = 3; B = 5 -> 243 (3⁵)
 //A = 2; B = 3 -> 8
 
-int PowNumber (int A, int B)
-{
-    if (B > 1)
-    {
-        return A * PowNumber (A, B - 1);
-    }
-    return A;
-}
+// int PowNumber (int A, int B)
+// {
+//     if (B > 1)
+//     {
+//         return A * PowNumber (A, B - 1);
+//     }
+//     return A;
+// }
 
-Console.WriteLine("Введите число ");
-int A = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите степень ");
-int B = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(PowNumber(A,B));
+// Console.WriteLine("Введите число ");
+// int A = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите степень ");
+// int B = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(PowNumber(A,B));
